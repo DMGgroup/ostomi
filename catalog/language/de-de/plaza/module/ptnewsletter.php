77904a8edad650cec372032e74ab7588ae041169
Vitaliy <hodachinskiy@gmail.com>
2020-03-25 16:59:33 +0200

    Initial commit

---
+++ catalog/language/de-de/plaza/module/ptnewsletter.php
@@ -0,0 +1,23 @@
+<?php
+// Newsletter Popup
+$_['heading_title1'] 	    = 'Sign Up Newsletter';
+$_['text_description'] 	    = 'Subscribe to the our store mailing list to receive updates on new arrivals, special offers and other discount information.';
+
+// Newsletter Block
+$_['heading_title2'] 	    = 'Sign up For Newsletter';
+$_['text_description2'] 	    = 'Register now to get updates on promotions & coupons.';
+
+// Text
+$_['text_close']            = 'Close';
+$_['text_notification']     = 'Don\'t show again';
+
+// Entry
+$_['entry_mail_subscribe']  = 'Please enter your email to subscribe';
+
+// Buttons
+$_['button_subscribe']      = 'Subscribe';
+
+// Messages
+$_['subscribe_success']     = 'Success: Your e-mail has been subscribed successfully!';
+$_['error_validate_mail']   = "Invalid E-mail Address!";
+$_['error_existed_mail']    = "This E-mail is existed already!";
